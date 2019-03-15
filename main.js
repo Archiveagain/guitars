d32d2d35c8cfb41e152fa6db6d04950784015a15
Lukasz <kobiel6@wp.pl>
2019-03-15 20:14:53 +0000

    1 commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Navbar Scroll Show*/
+$(function() {
+  $(document).scroll(function() {
+    const $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+/* Smooth Scroll */
